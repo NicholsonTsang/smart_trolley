1640f21783d10ec784a23a30ffa1734d5febe812
nicholsontsang <u3547738@connect.hku.hk>
2020-06-27 22:49:56 +0800

    move sensor code to AutoNavControlFragment for better achitecture, implement the start and stop sensor service function

--- app/src/main/java/com/hackasia/smart_trolley/AutoNavControlFragment.java
+++ app/src/main/java/com/hackasia/smart_trolley/AutoNavControlFragment.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,25 +16,73 @@ import androidx.fragment.app.Fragment;
 
 import util.SensorListener;
 
-public class AutoNavControlFragment extends Fragment {
+public class AutoNavControlFragment extends Fragment implements SensorEventListener{
+
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+    Sensor directionsensor;
+    Sensor gravitysensor;
+    SensorListener sensorlistener = new SensorListener();
+    double speed = 0;
+    double direction = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.auto_nav_state,container,false);
 
 
-
+        //register the sensor service and bind to the main activity
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        directionsensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        gravitysensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
 /***
+        sensorManager.registerListener((SensorEventListener) this,accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener((SensorEventListener) this,directionsensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener((SensorEventListener) this,gravitysensor, SensorManager.SENSOR_DELAY_NORMAL);
+***/
         //setting onclick listener for the start button
         v.findViewById(R.id.startbtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                sensorManager.registerListener((SensorEventListener) AutoNavControlFragment.this,accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+                sensorManager.registerListener((SensorEventListener) AutoNavControlFragment.this,directionsensor, SensorManager.SENSOR_DELAY_NORMAL);
+                sensorManager.registerListener((SensorEventListener) AutoNavControlFragment.this,gravitysensor, SensorManager.SENSOR_DELAY_NORMAL);
 
                 TextView speed = (TextView)v.findViewById(R.id.speedvaluetxt);
-
             }
         });
-***/
+
+        v.findViewById(R.id.stopbtn).setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                sensorManager.unregisterListener(AutoNavControlFragment.this);
+            }
+        });
+
         return v;
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
+            speed = sensorlistener.linearAccChange(sensorEvent);
+            //Log.d("test", Double.toString(speed));
+        }
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_GRAVITY)
+            sensorlistener.gravityChange(sensorEvent);
+        if(sensorEvent.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD)
+            sensorlistener.mFieldChange(sensorEvent);
+        if(sensorlistener.getGravity()!=null && sensorlistener.getMfield()!=null){
+            direction = sensorlistener.directionCalculation();
+            Log.d("test", Double.toString(direction));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
--- app/src/main/java/com/hackasia/smart_trolley/MainActivity.java
+++ app/src/main/java/com/hackasia/smart_trolley/MainActivity.java
@@ -15,18 +15,11 @@ import android.util.Log;
 
 import util.SensorListener;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+public class MainActivity extends AppCompatActivity{
 
     Fragment googleMapFragment = new GoogleMapFragment();
     Fragment autoNavFragment = new AutoNavControlFragment();
-    private static final String TAG = "MainActivity";
-    private SensorManager sensorManager;
-    Sensor accelerometer;
-    Sensor directionsensor;
-    Sensor gravitysensor;
-    SensorListener sensorlistener = new SensorListener();
-    double speed = 0;
-    double direction = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,50 +32,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         transaction.add(R.id.container,autoNavFragment);
         transaction.commit();
 
-
-        //register the sensor service and bind to the main activity
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        directionsensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        gravitysensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-
-        sensorManager.registerListener((SensorEventListener) MainActivity.this,accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener((SensorEventListener) MainActivity.this,directionsensor, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener((SensorEventListener) MainActivity.this,gravitysensor, SensorManager.SENSOR_DELAY_NORMAL);
-
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
 
-        if (sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
-            speed = sensorlistener.linearAccChange(sensorEvent);
-            Log.d(TAG, Double.toString(speed));
-        }
-        if(sensorEvent.sensor.getType() == Sensor.TYPE_GRAVITY)
-            sensorlistener.gravityChange(sensorEvent);
-        if(sensorEvent.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD)
-            sensorlistener.mFieldChange(sensorEvent);
-        if(sensorlistener.getGravity()!=null && sensorlistener.getMfield()!=null){
-            direction = sensorlistener.directionCalculation();
-            //Log.d(TAG, Double.toString(direction));
-        }
 
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
-/***
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        sensorlistener.onSensorChangedListener(sensorEvent);
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
-    ***/
 }
